9aac9705f067b6a632b53e64116b3c9b1de26bd2
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-22 18:42:52 +0200

    Format

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let ids = [];
 const demoP = document.getElementById("ytembed");
-$.get("yt.txt", function(data) {
+$.get("yt.txt", function (data) {
   const ids = data.split("\n");
   ids.reverse();
-  $.each(ids, function(index, item) {
+  $.each(ids, function (index, item) {
     const itemid = item.toString().split("=");
     console.log(itemid[1]);
     $("#ytembed").append(
@@ -16,34 +16,40 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-setTimeout(function(){
-( function() {
+setTimeout(function () {
+  (function () {
+    const youtube = document.querySelectorAll(".youtube");
 
-  const youtube = document.querySelectorAll( ".youtube" );
-  
-  for (var i = 0; i < youtube.length; i++) {
-      
-      const source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/hqdefault.jpg";
-      
+    for (var i = 0; i < youtube.length; i++) {
+      const source =
+        "https://img.youtube.com/vi/" +
+        youtube[i].dataset.embed +
+        "/hqdefault.jpg";
+
       const image = new Image();
-              image.src = source;
-              image.addEventListener( "load", function() {
-                  youtube[i].appendChild( image );
-              }(i) );
-      
-              youtube[i].addEventListener( "click", function() {
+      image.src = source;
+      image.addEventListener(
+        "load",
+        (function () {
+          youtube[i].appendChild(image);
+        })(i)
+      );
 
-                  const iframe = document.createElement( "iframe" );
+      youtube[i].addEventListener("click", function () {
+        const iframe = document.createElement("iframe");
 
-                          iframe.setAttribute( "frameborder", "0" );
-                          iframe.setAttribute( "allowfullscreen", "" );
-                          iframe.setAttribute( "src", "https://www.youtube-nocookie.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "");
+        iframe.setAttribute(
+          "src",
+          "https://www.youtube-nocookie.com/embed/" +
+            this.dataset.embed +
+            "?rel=0&showinfo=0&autoplay=1"
+        );
 
-                          this.innerHTML = "";
-                          this.appendChild( iframe );
-              } );    
-  }
-  
-} )();
+        this.innerHTML = "";
+        this.appendChild(iframe);
+      });
+    }
+  })();
 }, 2000);
-
