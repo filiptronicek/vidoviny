84308feb7d1c24935440642821f0d52d4bc8289c
Filip Troníček <filip.tronicek@seznam.cz>
2020-10-16 08:41:25 +0200

    Convert everything to synchronous anonymous arrow funcitons

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 let ids = [];
 
 const demoP = document.getElementById("ytembed");
-$.get("yt.txt", function (data) {
+$.get("yt.txt", (data) => {
     const ids = data.split("\n");
     ids.reverse();
-    $.each(ids, function (index, item) {
-        const itemid = item.toString().split("=");
-        console.log(itemid[1]);
-        $("#ytembed").append(`<div class="youtube" data-embed="${
-            itemid[1]
-        }"></div> <br /><br />`);
+    $.each(ids, (index, item) => {
+      const itemid = item.toString().split("=");
+      console.log(itemid[1]);
+      $("#ytembed").append(`<div class="youtube" data-embed="${itemid[1]}"></div> <br /><br />`);
     });
-});
+  });
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
-setTimeout(function () {
-    (function () {
-        const youtube = document.querySelectorAll(".youtube");
+setTimeout(() => {
+    (() => {
+      const youtube = document.querySelectorAll(".youtube");
 
-        for (let i = 0; i < youtube.length; i++) {
-            const source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed + "/hqdefault.jpg";
+      for (let i = 0; i < youtube.length; i++) {
+        const source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed + "/hqdefault.jpg";
 
-            const image = new Image();
-            image.src = source;
-            image.addEventListener("load", (function () {
-                youtube[i].appendChild(image);
-            })(i));
+        const image = new Image();
+        image.src = source;
+        image.addEventListener("load", (() => {
+          youtube[i].appendChild(image);
+        })(i));
 
-            youtube[i].addEventListener("click", function () {
-                const iframe = document.createElement("iframe");
+        youtube[i].addEventListener("click", () => {
+          const iframe = document.createElement("iframe");
 
-                iframe.setAttribute("frameborder", "0");
-                iframe.setAttribute("allowfullscreen", "");
-                iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
+          iframe.setAttribute("frameborder", "0");
+          iframe.setAttribute("allowfullscreen", "");
+          iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
 
-                this.innerHTML = "";
-                this.appendChild(iframe);
-            });
-        }
+          this.innerHTML = "";
+          this.appendChild(iframe);
+        });
+      }
     })();
-}, 2000);
+  }, 2000);
